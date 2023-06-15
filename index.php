1fd4d6139fa65ad0557954141948af1d8be24a85
Arda Altunel <ardaltunelmain@gmail.com>
2023-06-15 04:34:08 +0300

    ardaltunel

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Billiards Pool - Arda Altunel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<section>
+    <div class="score"></div>
+    <div class="canvas"></div>
+    <div class="message"></div>
+</section>
+
+<script src="https://cdn.rawgit.com/schteppe/poly-decomp.js/1ef946f1/build/decomp.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.12.0/matter.min.js"></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/111863/perlin.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
